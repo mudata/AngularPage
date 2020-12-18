eee6c44b15ee3b0ee547ad9949aa0ad6882a9d2e
MartinBorisov <mudata191@abv.bg>
2020-12-18 15:03:09 +0200

    nn

--- src/app/services/auth-gaurd.service.ts
+++ src/app/services/auth-gaurd.service.ts
@@ -1,11 +1,10 @@
 
 import { Injectable } from '@angular/core';
-import { CanActivate,CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService }      from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  //,CanActivateChild
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
@@ -18,13 +17,4 @@ export class AuthGuard implements CanActivate {
     return false;
   }
 
-  //  canActivateChild(
-  //   next: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): boolean {
-  //   if(this.authService.isAuthenticated){
-  //     return true;
-  //   }
-  //   this.router.navigate["/home"];
-  //   return false;
-  // }
 }
